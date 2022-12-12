d371add60b8c606ddbdacd20252136523b0d7374
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2022-12-12 17:25:53 +1100

    added CSS starter code and function to track time

--- assets/script.js
+++ assets/script.js
@@ -5,27 +5,53 @@ $("currenDay").html(todayDate);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(document).ready(function(){
+$(document).ready(function() {
     console.log("Ready!")
     // Get saved text from local storage
     varsavedText = localStorage.getItem("text");
-    if (savedText){
+    if (savedText) {
         savedText = JSON.parse(savedText);
     } else {
         savedText = {};
-    }},
+    };
     // saveBtn event listener
     $(".saveBtn").on("click", function(){
         // Get nearby values of the event listener
-        var eventText = $(this).siblings(".description").val();
-        var eventTime = $(this).parent().attr("id");
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
 
         // Save text in local storage
         savedText[time] = text;
         localStorage.setItem("text", JSON.stringify(savedText));
-})
-)
+    }),
 
+    function timeTracker(){
+        // Get current time
+        var timeNow = moment().hour();
+
+        // Loop over time blocks
+        $(".time-block").each(function(){
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            // To check the time and add the classes for the colour indicators
+            if (blockTime < timeNow){
+                $(this).addClass("past");
+            } else if (blockTime === timeNow){
+                $(this).addClass("present");
+            } else {
+                $(this).addClass("future");
+            }
+        });
+    },
+
+    // Get item from local storage
+    $(".time-block .description").each(function() {
+        var time = $(this).parent().attr("id");
+        $(this).val(savedText[time]);
+    });
+
+    timeTracker();
+});
 
 
     // TODO: Add a listener for click events on the save button. This code should
