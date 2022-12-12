39bb023cf34e7de210cf7475b5eca23ec10b446d
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2022-12-12 17:03:28 +1100

    added date and time, event listener and localStorage block

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,33 @@
+// Disaplay the current day and time
+var todayDate = moment().format('DD MMM, YYYY [at] hh:mm:ss a')
+$("currenDay").html(todayDate);
 
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
 $(document).ready(function(){
     console.log("Ready!")
-});
-
-$(function () {
+    // Get saved text from local storage
+    varsavedText = localStorage.getItem("text");
+    if (savedText){
+        savedText = JSON.parse(savedText);
+    } else {
+        savedText = {};
+    }},
+    // saveBtn event listener
     $(".saveBtn").on("click", function(){
-        var eventText = $(this).prev();
-        var timeLabel = $(this).prev().prev();
+        // Get nearby values of the event listener
+        var eventText = $(this).siblings(".description").val();
+        var eventTime = $(this).parent().attr("id");
+
+        // Save text in local storage
+        savedText[time] = text;
+        localStorage.setItem("text", JSON.stringify(savedText));
+})
+)
 
 
-    })
+
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -28,5 +46,5 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
   
